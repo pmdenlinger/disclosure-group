8d69e1e5308a329f0a57b29ff72e8a572f5fe6ed
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 19:58:55 -0700

    Initial Commit

---
+++ disclosure-group/disclosure_groupApp.swift
@@ -0,0 +1,17 @@
+//
+//  disclosure_groupApp.swift
+//  disclosure-group
+//
+//  Created by user on 6/21/21.
+//
+
+import SwiftUI
+
+@main
+struct disclosure_groupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
